2f60a7217ba5582312a6a05b29382f65d1dfe644
John Wiebalk <jwiebalk@github.com>
2020-04-30 11:17:40 -0400

    automate token

--- collectd/entrypoint.sh
+++ collectd/entrypoint.sh
@@ -6,7 +6,7 @@ sleep 5
 
 sed -i "s/{{HOSTNAME}}/$HOSTNAME/g" /etc/collectd/collectd.conf
 sed -i "s:{{basedir}}:$basedir:g" /etc/collectd/collectd.conf
-
+sed -i "s/{{SPLUNK_TOKEN}}/$SPLUNK_TOKEN/g" /etc/collectd/collectd.d/network.conf
 
 sleep 60
 
